b98571b4a8ccfae75656adf7d92d535cca731872
srishti20543 <srishti20543@iiitd.ac.in>
2023-09-02 21:37:26 +0530

    Make roll no. iterable JSON for each student

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,11 @@
-const mysql = require('mysql')
 const xlsx = require('xlsx')
+const filePath = "../data/Student_Database_2019.xlsm";
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user:"root",
-    password: 'adminiiitd',
-    database: "studentdb"
-})
-
-const filePath = "data/Student_Database_2019.xlsm";
 let workbook = xlsx.readFile(filePath);
 // Picks the first sheet from the Excel file.
 let worksheet = workbook.Sheets[workbook.SheetNames[0]];
 // Index the excel sheet data starting from zero index.
 let range = xlsx.utils.decode_range(worksheet["!ref"]);
-
 let data = [];
 
 for (let row = range.s.r; row <= range.e.r; row++) {
@@ -30,4 +21,29 @@ for (let row = range.s.r; row <= range.e.r; row++) {
     data.push(rowData);
 }
 
-console.log(data);
+const rollNumberMapping = {};
+  
+// Iterate through the data and construct the mapping
+data.forEach((row) => {
+    const [sn, rollNo, studentName, program, termCode, courseCode, course, credit, grade, spi, cpi] = row;
+  
+    // Check if the roll number already exists in the mapping
+    if (!rollNumberMapping[rollNo]) {
+      // If it doesn't exist, create a new entry with student information
+      rollNumberMapping[rollNo] = {
+        studentName,
+        program,
+        courses: [],
+      };
+    }
+  
+    // Add the course information to the courses array
+    rollNumberMapping[rollNo].courses.push({
+      courseCode,
+      grade,
+      termCode,
+      credit,
+    });
+  });
+
+console.log(rollNumberMapping[2018232]);
