737f541add134ff2a9185632b1cb3b7726179394
Tanishk Goyal <goyaltanishk02@gmail.com>
2023-09-03 00:46:47 +0530

    feat: packaged everything neatly, and transferred execution to index.ts

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,14 +1,6 @@
 import { StudentCourse } from "./degree";
 
 const xlsx = require("xlsx");
-const filePath = "data/Student_Database_2019.xlsm";
-
-let workbook = xlsx.readFile(filePath);
-// Picks the first sheet from the Excel file.
-let worksheet = workbook.Sheets[workbook.SheetNames[0]];
-// Index the excel sheet data starting from zero index.
-let range = xlsx.utils.decode_range(worksheet["!ref"]);
-let data = [];
 
 export type StudentInfo = {
   studentName: string;
@@ -20,57 +12,67 @@ export type DatabaseMap = {
   [key: number]: StudentInfo; // Here, specify the types for keys and values
 };
 
-const studentDatabase: DatabaseMap = {};
+export function getStudentDatabase(filePath: string): DatabaseMap {
+  const studentDatabase: DatabaseMap = {};
 
-for (let row = range.s.r; row <= range.e.r; row++) {
-  let rowData = [];
-  for (let col = range.s.c; col <= range.e.c; col++) {
-    let cell = worksheet[xlsx.utils.encode_cell({ r: row, c: col })];
-    // Checks if the cell exists.
-    rowData.push(cell ? cell.v : undefined);
+  let workbook = xlsx.readFile(filePath);
+  // Picks the first sheet from the Excel file.
+  let worksheet = workbook.Sheets[workbook.SheetNames[0]];
+  // Index the excel sheet data starting from zero index.
+  let range = xlsx.utils.decode_range(worksheet["!ref"]);
+  let data = [];
+
+  for (let row = range.s.r; row <= range.e.r; row++) {
+    let rowData = [];
+    for (let col = range.s.c; col <= range.e.c; col++) {
+      let cell = worksheet[xlsx.utils.encode_cell({ r: row, c: col })];
+      // Checks if the cell exists.
+      rowData.push(cell ? cell.v : undefined);
+    }
+    if (rowData.every((value) => value === undefined)) {
+      continue;
+    }
+    data.push(rowData);
   }
-  if (rowData.every((value) => value === undefined)) {
-    continue;
-  }
-  data.push(rowData);
+
+  // Iterate through the data and construct the mapping
+  data.forEach((row) => {
+    const [
+      _sn,
+      _rollNo,
+      _studentName,
+      _program,
+      _termCode,
+      _courseCode,
+      _course,
+      _credit,
+      _grade,
+      _spi,
+      _cpi,
+    ] = row;
+
+    // Check if the roll number already exists in the mapping
+    if (!studentDatabase[_rollNo]) {
+      // If it doesn't exist, create a new entry with student information
+      const thisStudent: StudentInfo = {
+        studentName: _studentName,
+        program: _program,
+        courses: [],
+      };
+      studentDatabase[_rollNo] = thisStudent;
+    }
+
+    const course: StudentCourse = {
+      courseCode: _courseCode,
+      grade: _grade,
+      semster: _termCode,
+      credit: _credit,
+    };
+
+    // Add the course information to the courses array
+    studentDatabase[_rollNo].courses.push(course);
+  });
+
+  return studentDatabase;
 }
 
-// Iterate through the data and construct the mapping
-data.forEach((row) => {
-  const [
-    _sn,
-    _rollNo,
-    _studentName,
-    _program,
-    _termCode,
-    _courseCode,
-    _course,
-    _credit,
-    _grade,
-    _spi,
-    _cpi,
-  ] = row;
-
-  // Check if the roll number already exists in the mapping
-  if (!studentDatabase[_rollNo]) {
-    // If it doesn't exist, create a new entry with student information
-    const thisStudent: StudentInfo = {
-      studentName: _studentName,
-      program: _program,
-      courses: [],
-    };
-    studentDatabase[_rollNo] = thisStudent;
-  }
-
-  const course: StudentCourse = {
-    courseCode: _courseCode,
-    grade: _grade,
-    semster: _termCode,
-    credit: _credit,
-  };
-
-  // Add the course information to the courses array
-  studentDatabase[_rollNo].courses.push(course);
-});
-
-console.log(studentDatabase[2018232]);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello World");
+import { get } from "http";
+import { DatabaseMap, getStudentDatabase } from "./database";
+
+const filePath = "data/Student_Database_2019.xlsm";
+
+const studentDatabase: DatabaseMap = getStudentDatabase(filePath);
+
+console.log(studentDatabase[2018232]);
