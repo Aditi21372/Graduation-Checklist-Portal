00577924a90040e5a0b58d6cb9b45fbaca8125eb
Diksha Sethi <dikshasethi251@gmail.com>
2023-10-06 14:50:02 +0530

    chore: added back button and non existent roll no

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: "app-dashboard",
-  templateUrl: "./dashboard.component.html",
-  styleUrls: ["./dashboard.component.css"],
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
   rollNumber: number = 0;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
     // Get the rollNumber parameter from the route
     this.route.params.subscribe((params) => {
-      this.rollNumber = params["rollNumber"];
+      this.rollNumber = params['rollNumber'];
     });
+  }
+
+  goBack() {
+    this.router.navigate(['/student-info-input']);
   }
 }
--- frontend/src/app/student-info-input/student-info-input.component.ts
+++ frontend/src/app/student-info-input/student-info-input.component.ts
@@ -1,18 +1,42 @@
-import { Component } from "@angular/core";
-import { Router } from "@angular/router";
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { StudentServiceService } from '../student-service.service';
 
 @Component({
-  selector: "app-student-info-input",
-  templateUrl: "./student-info-input.component.html",
-  styleUrls: ["./student-info-input.component.css"],
+  selector: 'app-student-info-input',
+  templateUrl: './student-info-input.component.html',
+  styleUrls: ['./student-info-input.component.css'],
 })
 export class StudentInfoInputComponent {
   studentRollNumber: number = 0;
   showContent: boolean = true;
-  constructor(private router: Router) {}
+  rollNumberExists: boolean = false;
+  showMessage: string = '';
+  constructor(
+    private router: Router,
+    private studentService: StudentServiceService
+  ) {}
+
+  ngOnInit() {
+    this.rollNumberExists = false;
+    this.showMessage = '';
+  }
 
   onSubmit() {
-    this.showContent = false;
-    this.router.navigate(["/dashboard", this.studentRollNumber]);
+    this.studentService
+      .getStudentData(this.studentRollNumber.toString())
+      .subscribe(
+        (data) => {
+          this.rollNumberExists = true;
+          this.showContent = false;
+          this.showMessage = '';
+          this.router.navigate(['/dashboard', this.studentRollNumber]);
+        },
+        (error) => {
+          this.showMessage = '';
+          this.rollNumberExists = false;
+          this.showMessage = "Roll number doesn't exist in the database!";
+        }
+      );
   }
 }
