4fdffa6307a7dd4cfbd73827b34844bf54894ea4
Diksha Sethi <dikshasethi251@gmail.com>
2023-11-23 23:39:59 +0530

    test: added tests for cw component

--- frontend/src/app/cw-details/cw-details.component.spec.ts
+++ frontend/src/app/cw-details/cw-details.component.spec.ts
@@ -44,4 +44,24 @@ describe('CwDetailsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize with queryParams', () => {
+    expect(component.studentName).toEqual('Test Student');
+    expect(component.program).toEqual('Test Program');
+    expect(component.rollNumber).toEqual(123);
+    expect(component.branch).toEqual('Test Branch');
+    expect(component.courseData).toEqual([
+      { course: 'Math', semester: 1, status: 'Completed', credits: 3, grade: 'A' },
+      { course: 'Physics', semester: 2, status: 'In Progress', credits: 4, grade: null }
+    ]);
+    // You can add more assertions based on your specific use case
+  });
+
+  it('should navigate back to dashboard on goBack()', () => {
+    component.goBack();
+    // Ensure that the navigate function is called with the correct arguments
+    const router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+    expect(router.navigate).toHaveBeenCalledWith(['/dashboard', component.rollNumber]);
+  });
+
 });
