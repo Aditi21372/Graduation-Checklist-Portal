8948b2700241d362bd9d3b95b29d36693d05bd0b
Diksha Sethi <dikshasethi251@gmail.com>
2023-11-23 16:26:48 +0530

    test: completed setup and created tests for frontend components

--- frontend/src/app/checklist/checklist.component.spec.ts
+++ frontend/src/app/checklist/checklist.component.spec.ts
@@ -1,21 +1,44 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { MatTableModule } from '@angular/material/table';
+import { StudentServiceService } from '../student-service.service';
+import { ChecklistComponent } from './checklist.component';
+import { of } from 'rxjs';
+import { MatCardModule } from '@angular/material/card';
 
-import { ChecklistComponent } from "./checklist.component";
-
-describe("ChecklistComponent", () => {
+describe('ChecklistComponent', () => {
   let component: ChecklistComponent;
   let fixture: ComponentFixture<ChecklistComponent>;
+  let studentService: jasmine.SpyObj<StudentServiceService>;
 
   beforeEach(() => {
+    studentService = jasmine.createSpyObj('StudentServiceService', [
+      'getStudentData',
+      'getMandatoryCourses',
+      'getBucketCourses',
+      'getSSHcourses',
+      'getCWcourses',
+      'getSGcourses',
+      'getBTPCredits',
+      'getTwoXXCredits',
+      'getIPCredits',
+      'getOnlineCourseCredits',
+      'get32Credits',
+      'getIncompleteGrade',
+    ]);
+
     TestBed.configureTestingModule({
       declarations: [ChecklistComponent],
-    });
+      imports: [MatCardModule, MatTableModule],
+      providers: [
+        { provide: StudentServiceService, useValue: studentService },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ChecklistComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it("should create", () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
