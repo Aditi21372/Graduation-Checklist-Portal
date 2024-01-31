328c9ddb7db5dd04d166f882eeb975b2eef9101b
srishti20543 <srishti20543@iiitd.ac.in>
2024-01-31 19:58:10 +0000

    sort

---
+++ frontend/src/app/utility.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UtilityService {
+
+
+  customSort(a: any, b: any): number {
+    const order = [
+      '1',
+      '2',
+      'Summer Term 1',
+      '3',
+      '4',
+      'Summer Term 2',
+      '5',
+      '6',
+      'Summer Term 3',
+      '7',
+      '8',
+      'Summer Term 4',
+      '9',
+      '10',
+      'Summer Term 5',
+      '11',
+      '12',
+    ];
+
+    // Convert a and b to strings if they are numbers
+    const strA = typeof a === 'number' ? a.toString() : a;
+    const strB = typeof b === 'number' ? b.toString() : b;
+
+    const indexA = order.indexOf(strA);
+    const indexB = order.indexOf(strB);
+
+    return indexA - indexB;
+  }
+}
